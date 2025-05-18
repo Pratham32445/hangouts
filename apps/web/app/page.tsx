5ff508acc627c348ba168a72c00e09065cdeaa3b
Pratham mehta <mehtapratham49@gmail.com>
2025-05-18 17:02:46 +0530

    started the frontend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,22 @@
-import Navbar from '@/components/landing/Navbar'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
 
-const Home = () => {
+const Index = () => {
   return (
-    <div className='max-w-5xl min-h-screen m-auto'>
-      <Navbar/> 
-    </div>
-  )
-}
+    <header className="max-w-6xl m-auto min-h-screen">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image src={"/logo.png"} width={100} height={50} alt="logo" />
+          <h1 className="text-3xl">Hangouts</h1>
+        </div>
+        <div className="flex gap-4"> 
+          <Button className="p-5 cursor-pointer">Get Started</Button>
+          <Button className="px-8 py-5 cursor-pointer border-none" variant={"outline"}>Login</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Home
+export default Index;
